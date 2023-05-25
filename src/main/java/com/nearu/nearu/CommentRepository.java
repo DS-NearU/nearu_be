56dbd3be764ec126fc6d789bfdc942597af37155
Dongeun Kim <de7danielkim@gmail.com>
2023-05-25 17:19:41 +0900

    dk second comment service

--- src/main/java/com/nearu/nearu/CommentRepository.java
+++ src/main/java/com/nearu/nearu/CommentRepository.java
@@ -2,7 +2,7 @@ package com.nearu.nearu;
 
 public interface CommentRepository
 {
-    boolean save (Comment c);
+    void save (Comment c);
     Comment fetch (Integer commentNo);
     void update (Comment c);
     void delete (Integer commentNo);

9e29e1130dc31bb45d5a362895720813fd4d44a7
Dongeun Kim <de7danielkim@gmail.com>
2023-05-25 17:17:12 +0900

    dk commentservice

--- src/main/java/com/nearu/nearu/ApplicationRepository.java
+++ src/main/java/com/nearu/nearu/ApplicationRepository.java
@@ -3,6 +3,6 @@ package com.nearu.nearu;
 public interface ApplicationRepository {
     boolean save(Application a);
     Application fetch(Integer adminNo);
-    void update (Application ㅁ);
+    void update (Application a);
     void delete (Integer adminNo);
 }

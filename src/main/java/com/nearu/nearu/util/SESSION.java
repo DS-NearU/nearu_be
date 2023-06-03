ef86b424f405e96a316b9da2ef6e5dc7fec42eb4
ahnjju73 <ahnjju73@gmail.com>
2023-06-03 21:16:20 +0900

    make it ez

---
+++ src/main/java/com/nearu/nearu/util/SESSION.java
@@ -0,0 +1,8 @@
+package com.nearu.nearu.util;
+
+public class SESSION {
+    public final static String TOKEN_NAME = "aimeets_token";
+    public final static String TOKEN_ISSURE = "aimeets_issure";
+    public final static String SESS_AUTH_KEY = "sess_auth_key";
+    public final static String SCHEME_SERVICE = "pubdata";
+}

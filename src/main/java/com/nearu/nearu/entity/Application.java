aca909c202b3410026237c5f387b81ad0c8f4341
ahnjju73 <ahnjju73@gmail.com>
2023-06-13 14:24:19 +0900

    entity edit

--- src/main/java/com/nearu/nearu/entity/Application.java
+++ src/main/java/com/nearu/nearu/entity/Application.java
@@ -1,20 +1,37 @@
 package com.nearu.nearu.entity;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@Entity
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Table(name = "application")
 public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_no")
     private Integer applicationNo;
+
+    @Column(name = "status")
     private Boolean status;
+
+    @Column(name = "due_date")
     private LocalDateTime dueDate;
     private LocalDateTime createdAt;
     private String location;
     private String conditions;
     private LocalDateTime dDay;
+
+    @ManyToOne
+    @Column(name = "admin_no")
     private Integer adminNo;
 
     public Application() {
